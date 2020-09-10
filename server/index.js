ad666eb20f866d73f8dd78270be994c79bfb400e
Krista Singleton <kris.singlet231@gmail.com>
2020-09-10 12:16:15 -0700

    first draft proxy

--- server/index.js
+++ server/index.js
@@ -1,47 +1,42 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
 const app = express();
 var cors = require('cors')
 app.use(cors())
 
 app.use(express.static(__dirname + '/../public'));
 
-// app.get('/:id', function(req, res) {
-//     res.sendFile(path.join(__dirname, '/../public/index.html'));
-// });
+const roomInfo = 'http://airbb3-info1-1972256759.us-west-1.elb.amazonaws.com';
+const roomBooking = 'http://sdcloadbalancer-1748024864.us-west-1.elb.amazonaws.com';
+const roomReviews = 'http://3bb-reviews-classic-1100595086.us-west-2.elb.amazonaws.com';
 
+let roomId = 0;
 
-app.get('/photos/:listingID', (req, res) => {
-    const { listingID } = req.params;
-    res.redirect(`http://localhost:3001/photos/${listingID}`);
-});
-
-app.get('/listings/:id', (req, res) => {
-    const { id} = req.params;
-    res.redirect(`http://localhost:3002/listings/${id}`);
-});
-
-app.get('/room/:id', (req, res) => {
-    const { id} = req.params;
-    res.redirect(`http://localhost:3003/room/${id}`);
-});
-
-app.get('/booking/:id', (req, res) => {
-    const { id} = req.params;
-    res.redirect(`http://localhost:3003/booking/${id}`);
-});
-
-app.get('/reviews/:room_id', (req, res) => {
-    const { room_id} = req.params;
-    res.redirect(`http://localhost:3004/reviews/${room_id}`);
-});
-
-app.get('/MoreHomes/:listingID', (req, res) => {
-    const { listingID} = req.params;
-    res.redirect(`http://localhost:3005/MoreHomes/${listingID}`);
-});
-
+  app.get('/listings/:id', (req, res) => {
+    const { id } = req.params;
+    roomId = id;
+    axios.get(`${roomInfo}/listings/${roomId}`)
+      .then(({ data }) => {res.send(data)});
+  });
+  app.get('/room/', (req, res) => {
+    axios.get(`${roomBooking}/room?id=${roomId}`)
+      .then(({ data }) => {res.send(data)});
+  });
+  app.get('/booking/', (req, res) => {
+    axios.get(roomBooking + `/booking?id=${roomId}`)
+      .then(({ data }) => {res.send(data)});
+  });
+  app.get('/rooms/:id/bundle.js', (req, res) => {
+    axios.get(`${roomReviews}/rooms/${roomId}/bundle.js`)
+      .then(({ data }) => {res.send(data)});
+  });
+   app.get('/reviews/', (req, res) => {
+    const { id } = req.params;
+    axios.get(`${roomReviews}/reviews/${roomId}`)
+      .then(({ data }) => {res.send(data)});
+  });
 
 app.listen(3010, function() {
     console.log('listening on port 3010!');
-  }); 
+});
